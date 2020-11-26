2fd20a039cbf59a5304c1259539f190a2f77eb1a
Koluw <semenov.stan@gmail.com>
2020-11-26 16:46:01 +0200

    change web_part
    change web_part from regular use to class. fixed some features of declaration and getters/setters.

--- web_part.py
+++ web_part.py
@@ -1,107 +1,107 @@
+"""
+Here we will build some visual environment, so next step will be creation
+of parameters needed to
+"""
 # global libraries
 from tkinter import *
 from tkinter import messagebox, filedialog
-# local modules
+from typing import Any
+
 from modules import auth_module as am
 
 
-def some_funct(p, some_element):
-    some_element.configure(text=p)
-    print(p)
-    pass
+def output_some(text, output_place):
+    output_place.configure(text=text)
 
 
-def reg():
+class MainApp:
     """
-    here we should send user/pass to auth_module
-    if there is a record like this -- OK
-    if not - depends on settings. we could let this user to create a new record.
-    find_user - should help with check. if type will be 1 - we found him.
-    if not - put_entry - function to create a new one.
-    The way to login - email/main. For email - first part, for main - second.
-    #############################################################################
-    This part is only for sending mails...
-    as a beginning we should find out what is the weeknum now. so we will knew
-    which letters we should create.
-    it seems this part should be in another module. with project_1 for example.
-    #############################################################################
-    This part is for working with project_1...
-    we should check if the user is available for using an app.
-    if yes - give him permissions he needs.
-    SO the main action - take a key "EMAIL" for this time.
-    Check if there is a record inside of config.file.
-    :return:
+        Authorize
+        PutNewEntry/UpdateEntry
+        CheckValues
+        StartIssues
+        BuildMenues - run inside of Init
     """
-    key = 'email'
-    res, some_arr = am.find_config(key)
-    if res == 1:
-        lbl.configure(text='success')
+    def __init__(self, main_window):
+        self.root = main_window
+        self.root.title("Sending Emails")
+        self.root.geometry('+300+450')
+        self.menu_create(self.root)
+        lbl = Label(self.root, text="Here we will try to combine some strings for sending an email. \n" +
+                                    "Please, register to get permissions")
+        lbl.grid(column=0, row=0, columnspan=3)
+        
+        # userid = StringVar()
+        
+        self.root.uid = Entry(self.root, width=10)  # , command=lambda: userid.set(uid.get())
+        self.root.uid.grid(column=1, row=1)
 
+        self.root.pwd = Entry(self.root, width=10)
+        self.root.pwd.grid(column=1, row=2)
+        
+        btn = Button(self.root, text="Print Some", command=lambda: output_some(text=self.root.uid.get() + ' : ' +
+                                                                                    self.root.pwd.get(),
+                                                                               output_place=lbl))
+        btn.grid(column=2, row=1)
+        
+        status_bar = Label(self.root, text='some test message for status bar')
+        status_bar.grid(sticky=N+E+W, columnspan=3)
 
-def new_entry():
-    uid_str = uid.get()
-    pwd_str = pwd.get()
-    if uid_str and pwd_str:
-        am.CONN_ARR['TCN'] = 'No'
-        am.CONN_ARR['DRV'] = '{SMTP Server}'
-        am.CONN_ARR['SRV'] = 'smtp.outlook.com'
-        am.CONN_ARR['DBN'] = 'imap.outlook.com'
-        am.CONN_ARR['UID'] = uid.get()
-        am.CONN_ARR['PWD'] = pwd.get()
+    def menu_create(self, window):
+        menu_var = Menu(window, tearoff=0)
+        window.config(menu=menu_var)
+        file_menu = Menu(menu_var, tearoff=0)
+        menu_var.add_cascade(label='File', menu=file_menu)
+        file_menu.add_command(label='New',  command=lambda: self.do_some('NewFile', 'create NewFile'))
+        file_menu.add_command(label='Open', command=lambda: self.do_some('OpenFile', 'open some File'))
+        file_menu.add_command(label='Exit', command=lambda: self.do_some('Exit', 'quit from App'))
+        ############################
+        register_menu = Menu(menu_var, tearoff=0)
+        menu_var.add_cascade(label='Register', menu=register_menu)
+        register_menu.add_command(label='Register',
+                                  command=lambda: self.do_some('Register', 'will try to login'))
+        ############################
+        help_menu = Menu(menu_var, tearoff=0)
+        menu_var.add_cascade(label='About', menu=help_menu)
+        help_menu.add_command(label='ABOUT', command=lambda: self.do_some('About', 'this is by STANUS'))
 
-        if am.put_entry(selector_key='email') == 1:
-            lbl.configure(text='success')
-    else:
-        lbl.configure(text='there are incorrect data')
+    def do_some(self, some_str, some_act):
+        messagebox.showinfo(title=some_str, message=some_act)
+        if some_str == 'Exit':
+            self.root.quit()
+        elif some_str == 'Register':
+            self.reg()
+        elif some_str == 'NewFile':
+            self.new_entry()
+            
+    def reg(self):
+        pass
 
+    def new_entry(self):
+        uid_str = self.root.uid.get()
+        pwd_str = self.root.pwd.get()
+        str_res = ''
+        if uid_str and pwd_str:
+            am.CONN_ARR['TCN'] = 'No'
+            am.CONN_ARR['DRV'] = '{SMTP Server}'
+            am.CONN_ARR['SRV'] = 'smtp.outlook.com'
+            am.CONN_ARR['DBN'] = 'imap.outlook.com'
+            am.CONN_ARR['UID'] = uid_str
+            am.CONN_ARR['PWD'] = pwd_str
+        
+            if am.put_entry(selector_key='email', conn_str=am.CONN_ARR) == 1:
+                str_res = 'success'
+        else:
+            str_res = 'there are incorrect data'
+        self.status_bar.configure(text=str_res)
+        
 
-def do_some(window, some_str, some_act):
-    messagebox.showinfo(title=some_str, message=some_act)
-    if some_str == 'Exit':
-        window.quit()
-    elif some_str == 'Register':
-        reg()
-    elif some_str == 'NewFile':
-        new_entry()
+if __name__ == '__main__':
+    root = Tk()
+    application = MainApp(root)
+    root.mainloop()
 
-
-def MenuCreate(main_window):
-    nav_menu = Menu(main_window)
-    main_window.config(menu=nav_menu)
-
-    file_menu = Menu(nav_menu, tearoff=0)
-    nav_menu.add_cascade(label='File', menu=file_menu)
-    file_menu.add_command(label='New', command=lambda: do_some(main_window, 'NewFile', 'create NewFile'))
-    # file_menu.add_command(label='Open', command=lambda: do_some(main_window, 'OpenFile', 'open some File'))
-    # file_menu.add_command(label='Exit', command=lambda: do_some(main_window, 'Exit', 'quit from App'))
-    ############################
-    register_menu = Menu(nav_menu, tearoff=0)
-    nav_menu.add_cascade(label='Register', menu=register_menu)
-    register_menu.add_command(label='Register', command=lambda: do_some(main_window, 'Register', 'will try to login'))
-    ############################
-    help_menu = Menu(nav_menu, tearoff=0)
-    nav_menu.add_cascade(label='About', menu=help_menu)
-    help_menu.add_command(label='ABOUT', command=lambda: do_some(main_window, 'About', 'this is by STANUS'))
-
-
-root = Tk()
-root.title("FirstApp")
-root.geometry('750x450+200+150')
-# root.
-
-MenuCreate(root)
-lbl = Label(root, text="Hello")
-lbl.grid(column=2, row=0)
-
-uid = Entry(root, width=10)
-uid.grid(column=1, row=1)
-
-pwd = Entry(root, width=10)
-pwd.grid(column=1, row=2)
-
-btn = Button(root, text="Print Some", command=lambda: some_funct(uid.get() + ' : ' + pwd.get(), lbl))
-btn.grid(column=2, row=1)
-
+#
+# root.title("Sending Emails")
+# root.geometry('750x450+500+450')
 # file_path = filedialog.askopenfilename()
-
-root.mainloop()
